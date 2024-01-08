b35b334207690032bf716ae36084a0235b833d2e
shubh9168 <149419729+shubh9168@users.noreply.github.com>
2024-01-09 00:37:12 +0530

    Add files via upload

---
+++ nastedforloop12.py
@@ -0,0 +1,84 @@
+
+#program1
+for i in range(3):
+    for j in range(3):
+        print("*",end="")
+    print()
+print()
+
+#program2
+num=1
+for i in range (4):
+    for j in range (4):
+        print(num,end=" ")
+        num=num+1
+    print()
+
+#program3
+for i in range (4):
+    
+    for j in range (4):
+        print(i + 1, end=" ")
+        
+    
+    print()
+
+#program4
+for i in range (4):
+    num=1
+    for j in range (4):
+        print(num,end=" ")
+        num=num+1
+    print()
+
+#program5
+
+for i in range(1 , 5):
+   
+    for j in range(0 , 4):
+        
+        print(i + j, end=" ")
+    print()
+
+#program6
+    
+for i in range(5):
+   
+    for j in range(5):
+        if i % 2==0:
+            print("#",end=" ")
+        else:
+            print("@",end=" ")
+    print()
+
+#program7
+    
+for i in range(1,18,6):
+    for j in range(i,i+3):
+        print(j,end="\t")
+    print()
+
+#program8
+
+for i in range(3):
+    for j in range(1,6,2):
+        print(j,end=" ")
+    print()
+
+#program9
+
+for i in range(1,6,2):
+    for j in range(i,i+3):
+        print(j,end="  ")
+    print()
+print()
+    
+#program10
+
+for i in range(1, 18, 4):
+    for j in range(i, i + 4):
+        print((f'{j:<5}'j, end='\t')
+    print()
+        
+    
+    
